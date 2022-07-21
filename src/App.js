6debc08312fd26bd578f1cfc1df8b589affa6a3f
woojoung <ywj981217@naver.com>
2022-07-21 18:36:52 +0900

    react hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Example1 from "./components/Example1";
 import Example2 from "./components/Example2";
-
+import Example3 from "./components/Example3";
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Example1></Example1>
         <Example2 />
+        <Example3 />
       </header>
     </div>
   );
