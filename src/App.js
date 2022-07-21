9d9102bbc80a427b35b557553999f07ccaae1f22
woojoung <ywj981217@naver.com>
2022-07-21 18:27:36 +0900

    react-hooks-example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Example1 from "./components/Example1";
+import Example2 from "./components/Example2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Example1></Example1>
+        <Example2 />
       </header>
     </div>
   );
